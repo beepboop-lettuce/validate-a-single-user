a6cf2f16c621d447e3b5ceefc598ebc9a826fd46
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-03 12:43:13 -0600

    else if for cases- wrong user/wrong pswd

--- main.go
+++ main.go
@@ -6,19 +6,19 @@ import (
 )
 
 const (
-	nothingTyped := "Usage: [username] [password]"
-	wrongUser := "wrong username"
-	wrongPswd := "wrong password"
-	user := "jack"
-	pswd := "1888"
-
+	nothingTyped = "Usage: [username] [password]"
+	wrongUser    = "wrong username"
+	wrongPswd    = "wrong password"
+	user         = "jack"
+	pswd         = "1888"
 )
-
 
 func main() {
 	if len(os.Args[1]) < 3 {
 		fmt.Println(nothingTyped)
 	} else if os.Args[1] != user {
-		fmt.Printf()
+		fmt.Println(wrongUser)
+	} else if os.Args[2] != pswd {
+		fmt.Println(wrongPswd)
 	}
 }
