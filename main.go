57c30a12c9912fb3570797dc4b403d480496b041
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-03 12:35:58 -0600

    constants for different scenarios main func- if case for <3 chars typed

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-const ()
+import "os"
+
+const (
+	blank := "Usage: [username] [password]",
+	wrongUser := "wrong username",
+	wrongPswd := "wrong password",
+	user := "jack",
+	pswd := "1888",
+
+)
+
 
 func main() {
-
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %q\n", blank)
+	}
 }
