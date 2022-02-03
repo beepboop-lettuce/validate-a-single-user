ce1bfc799ef6a84140ce117c8f5034506fe996e8
beepboop-lettuce <ericaharris0486@gmail.com>
2022-02-03 13:41:02 -0600

    added a second user fixed syntax errors fixed logic- only correct combos of unam and pswd will work

--- main.go
+++ main.go
@@ -7,20 +7,25 @@ import (
 
 const (
 	nothingTyped = "Usage: [username] [password]"
-	denied       = "Wrong username or password, try again"
+	deniedUname  = "Access denied, wrong username"
+	deniedPswd   = "Access denied, wrong password"
 	user1, user2 = "jack", "quentin"
 	pswd1, pswd2 = "1888", "1234"
-	granted      = "Access granted!"
+	granted      = "Access granted for %q!\n"
 )
 
 func main() {
-	if len(os.Args) < 3 {
+	args := os.Args
+
+	if len(args) != 3 {
 		fmt.Println(nothingTyped)
-	} else if os.Args[1] == user1 && os.Args[2] == pswd1 {
-		fmt.Println(granted)
-	} else if os.Args[1] == user2 && os.Args[2] == pswd2 {
-		fmt.Println(granted)
+	} else if args[1] == user1 && args[2] == pswd1 {
+		fmt.Printf(granted, args[1])
+	} else if args[1] == user2 && args[2] == pswd2 {
+		fmt.Printf(granted, args[1])
+	} else if args[1] != user1 && args[1] != user2 {
+		fmt.Println(deniedUname)
 	} else {
-		fmt.Println(denied)
+		fmt.Println(deniedPswd)
 	}
 }
